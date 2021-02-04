f6ec4696b7be8fb2ed6431c316a26a7d914f52fc
Marl-l <marltalalaev@gmail.com>
2021-02-04 19:41:44 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var mySwiper = new Swiper(".swiper-container", {
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+
+  const swiper = new Swiper(".review .swiper-container", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination2",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+});
